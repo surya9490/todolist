271760fa1d3c3b4e15d649337db9a4d16e1b248c
surya9490 <psuryakin@gmail.com>
2022-11-13 05:06:52 +0000

    code update

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
+const cors = require("cors");
 
 const databasePath = path.join(__dirname, "todos.db");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 let database = null;
 
